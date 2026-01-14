6e0504de10d2fea95d067e6f3792d324a6eb0a07
John Mars <johnmars@192.168.1.7>
2026-01-14 06:40:42 +0200

    just added readme.md and test.py script.

---
+++ test.py
@@ -0,0 +1,11 @@
+def pythagorian_theorem(a, b):
+    """Calculate the length of the hypotenuse of a right triangle.
+
+    Args:
+        a (float): Length of one leg of the triangle.
+        b (float): Length of the other leg of the triangle.
+
+    Returns:
+        float: Length of the hypotenuse.
+    """
+    return (a**2 + b**2) ** 0.5
